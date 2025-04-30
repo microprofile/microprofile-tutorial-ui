b9290416a186e978aefe305582cec06423f56b46
Tarun Telang <tarun.telang@gmail.com>
2025-04-30 05:52:42 +0530

    Adding /src folder

---
+++ src/js/vendor/highlight.bundle.js
@@ -0,0 +1,20 @@
+;(function () {
+  'use strict'
+
+  var hljs = require('highlight.js/lib/highlight')
+  hljs.registerLanguage('asciidoc', require('highlight.js/lib/languages/asciidoc'))
+  hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'))
+  hljs.registerLanguage('css', require('highlight.js/lib/languages/css'))
+  hljs.registerLanguage('java', require('highlight.js/lib/languages/java'))
+  hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
+  hljs.registerLanguage('json', require('highlight.js/lib/languages/json'))
+  hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'))
+  hljs.registerLanguage('none', require('highlight.js/lib/languages/plaintext'))
+  hljs.registerLanguage('properties', require('highlight.js/lib/languages/properties'))
+  hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'))
+  hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
+  hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'))
+  ;[].slice.call(document.querySelectorAll('pre code.hljs[data-lang]')).forEach(function (node) {
+    hljs.highlightBlock(node)
+  })
+})()
